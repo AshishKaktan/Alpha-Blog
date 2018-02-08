c3a291c9ec94f5f85b45d2258e6a6ba8461c1a21
kaktan.ashish <kaktan.ashish@gmail.com>
2018-02-08 05:13:08 -0500

    Added Edit Option

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,6 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article = Article.new(article_params)
     
     if @article.save
       flash[:notice] = "Article was successfully created"
@@ -16,12 +15,26 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updates"
+      redirect_to article_path(@article);
+    else
+      render 'edit'
+    end
   end
   
   private
     def article_params
        params.require(:article).permit(:title,:description)
     end
-end 
+end
